8abfd7b1ce5a6f88f01bffa9ed94cca51d74a3ec
Swyter <swyterzone@gmail.com>
2020-04-03 14:08:30 +0200

    Turns out there's a pyYAML parameter to avoid sorting dictionaries and their keys. Start filling out the YAML structure tree for each sound effect, still with a lot of dummy data.

--- eurosound-unpack.py
+++ eurosound-unpack.py
@@ -2,6 +2,7 @@ import yaml
 import io
 import struct
 import os
+import collections
 
 from pathlib import Path
 
@@ -74,6 +75,11 @@ for file_path in Path(sfx_folder).glob('HC*.SFX'):
         print(' ', sfxcount, 'sound effects here')
         
         for i in range(0, sfxcount):
+        
+            f.seek(sfxstart + 4 + (i * 0x8))
+            
+            print("%x" % f.tell())
+            
             hashcode        = struct.unpack('<I', f.read(4))[0]
             offset          = struct.unpack('<I', f.read(4))[0]
             
@@ -88,12 +94,33 @@ for file_path in Path(sfx_folder).glob('HC*.SFX'):
             
             sfx.append(hc_str)
             
+            f.seek(sfxstart + offset)
+            
+            d = {}#collections.OrderedDict()
+            
+            d['params'] = {}
+            d['params']['duckerLength']      = struct.unpack('<H', f.read(2))[0]
+            d['params']['auckerLength']      = struct.unpack('<H', f.read(2))[0]
+            d['minDelay']          = struct.unpack('<H', f.read(2))[0]
+            d['maxDelay']          = struct.unpack('<H', f.read(2))[0]
+            d['innerRadiusReal']          = struct.unpack('<H', f.read(2))[0]
+            d['outerRadiusReal']          = struct.unpack('<H', f.read(2))[0]
+            d['reverbSend']          = struct.unpack('<H', f.read(2))[0]
+            d['trackingType']          = struct.unpack('<H', f.read(2))[0]
+            d['maxVoices']          = struct.unpack('<H', f.read(2))[0]
+            d['priority']          = struct.unpack('<H', f.read(2))[0]
+            d['ducker']          = struct.unpack('<H', f.read(2))[0]
+            d['flags']          = struct.unpack('<H', f.read(2))[0]
+            d['samples']          = struct.unpack('<H', f.read(2))[0]
+            
+            
             if not os.path.exists(hc_str):
                 os.mkdir(hc_str)
                 
             with open(hc_str + '/effectProperties.yml', 'w') as outfile:
                 outfile.write('# swy: EngineX sound effect exported from %s / %#x\n' % (hc_str, 0x1A000000 | hashcode))
-                yaml.dump([ 'stuff' ], outfile, default_flow_style=False)
+                yaml.dump(d, outfile, default_flow_style=False, sort_keys=False)
+                
 
     with open(ht[hash] + '.yml', 'w') as outfile:
         outfile.write('# swy: EngineX sound bank exported from %s / %#x\n' % (ht[hash], 0x1c000000 | hash))
